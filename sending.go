7b7a64bfd060d91ae3829e855f5765ce718d0547
Donovan Diamond <56517875+DonovanDiamond@users.noreply.github.com>
2025-04-30 19:31:31 +0200

    return message directly after sending and make callback optional

--- sending.go
+++ sending.go
@@ -8,22 +8,24 @@ import (
 	"go.mau.fi/whatsmeow/types"
 )
 
-func (conn *Connection) SendMessage(message Message) error {
+func (conn *Connection) SendMessage(message Message, sendOnCallback bool) (Message, error) {
 	out := waE2E.Message{
 		Conversation: message.ContentBody,
 	}
 	jid, err := types.ParseJID(message.ChatJID)
 	if err != nil {
-		return fmt.Errorf("failed to parse ChatJID: %w", err)
+		return message, fmt.Errorf("failed to parse ChatJID: %w", err)
 	}
 	resp, err := conn.client.SendMessage(context.Background(), jid, &out)
 	if err != nil {
-		return fmt.Errorf("failed to send message: %w", err)
+		return message, fmt.Errorf("failed to send message: %w", err)
 	}
 	message.MessageID = resp.ID
 	senderJID := resp.Sender.String()
 	message.SenderJID = &senderJID
 	message.Timestamp = &resp.Timestamp
-	conn.Callbacks.Message(message)
-	return nil
+	if sendOnCallback {
+		conn.Callbacks.Message(message)
+	}
+	return message, nil
 }
